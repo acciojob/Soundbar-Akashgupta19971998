60191dabff1633b66575a4895f3d77f71d04bcde
Akashgupta19971998 <akashguptaakash2@gmail.com>
2023-04-13 13:45:14 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
+const buttonA = document.querySelector("#btn1");
+const buttonB = document.querySelector("#btn2");
+const buttonC = document.querySelector("#btn3");
+const buttonD = document.querySelector("#btn4");
+const buttonE = document.querySelector("#btn5");
+const buttonF = document.querySelector("#btn6");
+const stop = document.querySelector("#stop");
+
+// console.log(buttonA.value) 
+
+function button1() {
+    audio.play();
+}
+//button1();
+function button2() {
+    audio.play();
+}
+function button3() {
+    audio.play();
+}
+function button4() {
+    audio.play();
+}
+function button5() {
+    audio.play();
+}
+function button6() {
+    audio.play();
+}
+function stops() {
+    audio.pause();
+}
